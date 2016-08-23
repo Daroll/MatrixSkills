07c2a714f75708a77f9b108c338adb43494a1b43
mkedzierska <mkedzierska@GK29RX1.SIIPOLSKA.PL>
2016-08-23 14:08:08 +0200

    Okno jest resizable, działa przycisk Close, jest ustawiony minimalny rozmiar okna.

--- MatrixSkills/src/window/MainWindow.java
+++ MatrixSkills/src/window/MainWindow.java
@@ -1,86 +1,88 @@
 package window;
 
-import java.awt.EventQueue;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 
 public class MainWindow {
-	
-	private JFrame frame;
-	private JButton btnNewUser, btnButton, btnClose;
-	private JMenu mFile, mUsers, mAbout, mSkills;
-	
-				// uruchomienie //
-	public static void main(String[] args){
-		 
-		 EventQueue.invokeLater(new Runnable()
-		    {
-		            @Override
-		            public void run()
-		            {
-		             MainWindow window = new MainWindow();
-		    window.frame.setVisible(true);
-		            }
-		    });
-		}
-public MainWindow(){
-	initialize(); 
-	}
+    final static boolean shouldFill = true;
+    final static boolean shouldWeightX = true;
+    final static boolean RIGHT_TO_LEFT = false;
 
-				// inicjalizacja ramki//
+    public static void addComponentsToPane(Container pane) {
+        if (RIGHT_TO_LEFT) {
+            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        }
 
-private void initialize() {
-	frame = new JFrame("Skills Matrix"); // nazwa okna
-	frame.setSize(300, 300);			//wielkosc okna 
-	frame.setLocationRelativeTo(null);	//
-	frame.setResizable(false);  		 //okno nie zmienia rozmiarow
-	
-	frame.getContentPane().setLayout(null); 
-	
-	//gorne menu
-	JMenuBar menuBar = new JMenuBar();
-	frame.setJMenuBar(menuBar);
-	
-	mFile = new JMenu("File");		//File Menu
-	menuBar.add(mFile);
-	
-	//dodwanie nowego pracownika
-	
-	btnNewUser = new JButton("New User");
-	btnNewUser.setBounds(75, 60,150,30);
-	btnNewUser.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			new NewUser();
-			frame.setVisible(false);
-			window.NewUser.main(null);
-			
-		}
-	});
-	btnNewUser.setToolTipText("Adds New User");
-	frame.getContentPane().add(btnNewUser);
-	
-	//cos tu bedzie
-	
-	btnButton = new JButton("Jakis knefel");
-	btnButton.setBounds(75,100,150,30);
-	frame.getContentPane().add(btnButton);
-	
-	//zamkniecie apki
-	
-	btnClose = new JButton("Close");
-	btnClose.setBounds(75,140,150,30);
-	frame.getContentPane().add(btnClose);
-	btnClose.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent arg0) {
-			System.exit(0);
-		}
-		});
-		btnClose.setToolTipText("Close App");
-		frame.getContentPane().add(btnClose);
-	}
+        JButton button;
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints buttonmainwindow = new GridBagConstraints();
+        if (shouldFill) {
+            //natural height, maximum width
+        	buttonmainwindow.fill = GridBagConstraints.HORIZONTAL;
+        }
+
+        button = new JButton("New User");
+        buttonmainwindow.ipady = 40; // make button tall for 40px 
+        buttonmainwindow.ipadx = 100; // make button width for 100px
+        buttonmainwindow.gridy = 0; // first row
+        buttonmainwindow.weighty = 1.0; 
+        buttonmainwindow.insets = new Insets(30,30,30,30);  //padding
+        
+        pane.add(button, buttonmainwindow);
+
+        button = new JButton("Knefel");
+        buttonmainwindow.ipady = 40;
+        buttonmainwindow.ipadx = 100;
+        buttonmainwindow.gridy = 1; // second row
+        buttonmainwindow.insets = new Insets(30,30,30,30);
+        buttonmainwindow.weighty = 1.0;
+        buttonmainwindow.weightx = 0.0;
+        buttonmainwindow.gridwidth = 3;
+        
+        pane.add(button, buttonmainwindow);
+
+        button = new JButton("Close");
+        buttonmainwindow.ipady = 40;
+        buttonmainwindow.ipadx = 100;
+        buttonmainwindow.insets = new Insets(30,30,30,30);
+        buttonmainwindow.gridy = 2;       //third row
+        pane.add(button, buttonmainwindow);
+        
+      	button.addActionListener(new ActionListener() {
+      		public void actionPerformed(ActionEvent arg0) {
+      			System.exit(0);
+      		}
+      		});
+        
+        
+    }
+    
+    
+
+    private static void createAndShowGUI() {
+
+        //Create and set up the window.
+        JFrame frame = new JFrame("Skills Matrix");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(300, 400));
+
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
